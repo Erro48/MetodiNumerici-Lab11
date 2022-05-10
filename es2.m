a41a0d4a659a65f7a4994b23709d233db16d59cc
Enrico Tagliaferri - enrico.tagliaferri2@studio.unibo.it <enrico.tagliaferri2@studio.unibo.it>
2022-05-10 11:03:43 +0200

    Create test for interpLagrange method

--- es2.m
+++ es2.m
@@ -1,3 +1,14 @@
 clc
 clear all
 
+n = 5;
+f = @(x) 1 ./ (1 + x.^2); 
+x = linspace(-5, 5, n+1);
+y = f(x);
+xv = linspace(-5, 5);
+
+pol = interpLagrange(x, y, xv)
+
+figure
+plot(xv, pol, 'r-', xv, f(xv), 'b:', x, y, 'o')
+legend('Pol Lagrange', 'Funzione esatta', 'Dati noti')
